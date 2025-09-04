2e8f0d17fd14690b94172cc84dc6e1fc87e7a0b9
sushma.ayenampudi@gmail.com <your-email@example.com>
2025-09-04 18:40:50 -0500

    fourth commit

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Declaring variables
+PERSON1=Sachin  # no space between = and value
+PERSON2=Rahul
+
+# Referring variables
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: I am fine too. How is your work?"
+echo "$PERSON2:: Not bad. I am thinking to upgrade to DevOps"
